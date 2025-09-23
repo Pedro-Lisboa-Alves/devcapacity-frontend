fcf4d628b9645d737a116ea52223125c2b628ff0
Pedro Alves <pl.lisboa@gmail.com>
2025-09-23 14:59:24 +0100

    Ajuste de estilo da tabel a de teams

--- DevCapacityWebApp/obj/Debug/net9.0/DevCapacityWebApp.AssemblyInfo.cs
+++ DevCapacityWebApp/obj/Debug/net9.0/DevCapacityWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d52d7a97ae3d39d53871a0d96f3d85c73bf36bdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7726c9c3a88d71a26fba072bcfaee3b7f360706")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
