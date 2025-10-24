bb808f9d86321b22c098c8bc84be2e299df60928
Pedro Alves <pl.lisboa@gmail.com>
2025-10-24 10:40:07 +0100

    Style corrections

--- DevCapacityWebApp/obj/Debug/net9.0/DevCapacityWebApp.AssemblyInfo.cs
+++ DevCapacityWebApp/obj/Debug/net9.0/DevCapacityWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40dc1f965eead9d3ad93fd9472da13c6ed42c84c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ed7a2b45af0c69ef6b59aca73105263ba88e26c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
