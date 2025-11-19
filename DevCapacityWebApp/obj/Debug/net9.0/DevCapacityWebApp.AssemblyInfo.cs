071e735b67687d2bdee05b79adf52e6e33daaf55
Pedro Alves <pl.lisboa@gmail.com>
2025-11-19 16:03:23 +0000

    Sync

--- DevCapacityWebApp/obj/Debug/net9.0/DevCapacityWebApp.AssemblyInfo.cs
+++ DevCapacityWebApp/obj/Debug/net9.0/DevCapacityWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940a6a3d349eab43ed48df832a89d70b79308f25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00e3d242d6e27bbeb0526a87ced070a7c3e4806")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevCapacityWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
